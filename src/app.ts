2f5ce3704f5354b9606793650a2abd09e82f8995
Aman Yadav <amandotyadav@gmail.com>
2026-06-27 03:06:15 +0530

    build: configure path aliases for production

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,7 @@
-export {};
+import express, { type Express } from "express";
+
+export function createApp(): Express {
+  const app = express();
+
+  return app;
+}
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,8 @@
-console.log("Backend Template Started...");
+import { createApp } from "@/app.js";
+
+const app = createApp();
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
